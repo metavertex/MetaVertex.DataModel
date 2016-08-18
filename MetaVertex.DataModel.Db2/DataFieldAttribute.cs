8aaeb1e3aa0cff67c2584e80cf884b8c162d9cb4
Toby Johnson <irontoby@gmail.com>
2016-08-18 10:22:14 -0400

    initial commit

---
+++ MetaVertex.DataModel.Db2/DataFieldAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaVertex.DataModel.Db2
+{
+    public sealed class DataFieldAttribute : Attribute
+    {
+        public DataFieldAttribute(int index)
+        {
+            if (index < 1)
+                throw new ArgumentException("Index must not be negative");
+
+            Index = index;
+        }
+
+        public DataFieldAttribute(string columnName)
+        {
+            if (string.IsNullOrEmpty(columnName))
+                throw new ArgumentException("Column name cannot be null or empty", nameof(columnName));
+
+            ColumnName = columnName;
+        }
+
+        public int Index { get; }
+
+        public string ColumnName { get; }
+    }
+}
